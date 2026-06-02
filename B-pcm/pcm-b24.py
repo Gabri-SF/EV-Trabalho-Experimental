d5aa10a8016738cd7257f299b2db19b089e2a2e4
Gabriel Leitão <gleitao147@gmail.com>
2026-06-02 12:07:19 +0100

    chore: updated B, final revision

--- B-pcm/pcm-b24.py
+++ B-pcm/pcm-b24.py
@@ -4,42 +4,28 @@ from scipy.signal import find_peaks
 import matplotlib.pyplot as plt
 import pandas as pd
 
+IMAGE_SIZE = (10, 3.5)
+SHOW_IMAGES = False
 
 
 def main(): 
     # read csv to dataframe
-    df = pd.read_csv("EV_2026.A24", sep=";")
+    df = pd.read_csv("EV_2026.B24", sep=";")
 
     t = df.iloc[:,0] # in seconds
-    
+
     sinais = list(df.columns.values)[1:]
     for i in range(len(sinais)):
         # in m /s^2
         acel_i = df.iloc[:, i + 1] # skip 0, cause is time
+
+        # 9. variacao temporal das grandezas
+        plot_sinal_graph(t, acel_i, sinais[i])
+
+        # 10. determinar e apresentar o espetro unilateral de amplitude
         (f, Y) = FFT(t, acel_i.values)
-        plot_sinal_graph(t, acel_i, sinais[i], f, Y)
+        plot_single_sided_spectrum(f, Y, sinais[i])
 
-        play_data_as_audio(acel_i.values, 1000)
-
-
-        
-
-
-def play_data_as_audio(data, sample_rate):
-    int16_audio = (data * 32767).astype(np.int16)
-    p = pyaudio.PyAudio()
-    stream = p.open(  
-        format=pyaudio.paInt16,  
-        channels=1,  
-        rate=sample_rate,  
-        output=True,  
-    ) 
-    stream.write(int16_audio.tobytes())
-
-    # Cleanup  
-    stream.stop_stream()  
-    stream.close()  
-    p.terminate()
 
 def FFT(t, y):
     N = len(t) # data length
@@ -51,13 +37,16 @@ def FFT(t, y):
 
     # FFT to get: single-sided magnitude spectrum
     Y = fft(y)
+    
     # To get single-sided magnitude spectrum, we need to do:
     # The two-sided amplitude spectrum, where the spectrum in the positive frequencies is the complex conjugate of the spectrum in the negative frequencies, has half the peak amplitudes of the time-domain signal.
     # Also, we need to rescale, dividing by N
     Y = 2 / N * np.abs(Y[:n_uni])
-    # no need to multiply by 2 because these amplitudes correspond to the zero and Nyquist frequencies, they dont have complex conjugate in negative freq
+    # DC (0 Hz) is unique in the FT
     Y[0] = Y[0] / 2
-    Y[-1] = Y[-1] / 2
+    # Nyquist frequency is unique for even N, so we shall not multiply by 2, so we revert the previous multiplication
+    if N % 2 == 0:
+        Y[-1] = Y[-1] / 2
     return (f, Y)
 
 
@@ -69,31 +58,42 @@ def get_freq_peaks(f, y, signal_name, height_threshold=0.05):
     return peaks_index, properties
 
 
-def plot_sinal_graph(t, signal, signal_name, f, ftt_signal):
+def plot_sinal_graph(t, signal, signal_name):
+    # Visualization
+    plt.figure(figsize=IMAGE_SIZE)
+
+    # Original signal
+    plt.plot(t, signal.values, linewidth=0.5)
+    plt.title("Variação temporal do sinal " + signal_name)
+    plt.xlabel("Tempo [s]")
+    plt.ylabel("Aceleracao [m/s^2]")
+    plt.grid()
+
+    plt.tight_layout()
+    plt.savefig("results/sinal-"+signal_name+".png")
+    if SHOW_IMAGES:
+        plt.show()
+
+
+def plot_single_sided_spectrum(f, ftt_signal, signal_name):
     
     # get peaks
     peaks_index, properties = get_freq_peaks(f, ftt_signal, signal_name)
 
     # Visualization
-    plt.figure(figsize=(10, 4))
-
-    # Original signal
-    plt.subplot(1, 2, 1)
-    plt.plot(t, signal.values)
-    plt.title("Variação temporal do sinal")
-    plt.xlabel("Tempo (s)")
-    plt.ylabel("Aceleracao (m/s^2)")
+    plt.figure(figsize=IMAGE_SIZE)
 
     # Spectral
-    plt.subplot(1, 2, 2)
     plt.plot(f, ftt_signal, '-', f[peaks_index],properties['peak_heights'],'x')
-    plt.title("Espetro unilateral de amplitude")
-    plt.xlabel("Freq (Hz)")
+    plt.title("Espetro unilateral de amplitude do sinal " + signal_name)
+    plt.xlabel("Freq [Hz]")
     plt.ylabel("Magnitude |X(freq)|")
+    plt.grid()
 
     plt.tight_layout()
-    plt.suptitle("Sinal " + signal_name)
-    plt.show()
+    plt.savefig("results/espetro-"+signal_name+".png")
+    if SHOW_IMAGES:
+        plt.show()
 
 
 if __name__ == "__main__":
